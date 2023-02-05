2a60017adf87f844288544f6dc1e1454c51615c2
mayagoldberg <mayag6995@gmail.com>
2023-02-05 20:15:48 +0200

    pre_processing

--- analysis_empirical_data/preprocessing.R
+++ analysis_empirical_data/preprocessing.R
@@ -1,7 +1,6 @@
 rm(list=ls())
 
-df<- read.csv('G:/My Drive/Xbox/data/empirical_data/df_raw.csv')
-df <- as.data.frame(df)
+df<- read.csv('./data/empirical_data/df_raw.csv')
 library(dplyr)
 
 df <-df %>% filter(block_type==' test')
@@ -9,16 +8,14 @@ df <-df %>% filter(block_type==' test')
 df= df %>% mutate(stay = (choice_card == lag(choice_card))*1, 
                   reward_oneback = lag(reward),
                   reoffer_chosen_oneback = (lag(choice_card) == left_offer | lag(choice_card) ==right_offer),
-                  # accuracy = (exp_value_chosen > exp_value_unchosen)*1,
+                  exp_value_unchosen = ifelse(exp_value_chosen == exp_value_left, exp_value_right,exp_value_left),
+                  accuracy = (exp_value_chosen > exp_value_unchosen)*1,
                   stay_key = (choice_key == lag(choice_key))*1,
-                  # delta_exp_value = abs(exp_value_chosen - exp_value_unchosen),
+                  delta_exp_value = abs(exp_value_chosen - exp_value_unchosen),
                   condition_oneback =lag(condition),
 )
 
-df= df %>% mutate(abort = ( rt < 0.2 | rt > 4 | is.na(rt) | trial ==1 ))
-                 
-# df <-df %>% filter(block == lag(block))
-
+df= df %>% mutate(abort = ( rt < 0.2 | rt > 4 | is.na(rt)))
 
 #######omit subjects with more the 90% stay for key ----------------------
 
@@ -35,8 +32,8 @@ print('no subjects were ommited, all had pstay_key<0.9')
 p.abort=df%>%group_by(subject)%>%summarise(p.abort=mean(abort))%>%mutate(above_threshold=(p.abort>.25))
 sum(p.abort$above_threshold)
 
-subj.list=p.abort$subject[p.abort$above_threshold==0]
-df=df%>%filter(subject%in%subj.list)
+#subj.list=p.abort$subject[p.abort$above_threshold==0]
+#df=df%>%filter(subject%in%subj.list)
 
 print('we ommited 0 participants since they had more then 25% trial ommited due to fast/slow RTs')
 
@@ -44,7 +41,7 @@ print('we ommited 0 participants since they had more then 25% trial ommited due 
 #######filter ----------------------
 
 mean(df$abort)
-print('we then ommited 4% of trials due to fast/slow rts or first trial in the block')
+print('we then ommited 2% of trials due to fast/slow rts or first trial in the block')
 
 df=df%>%filter(abort==0)
 
@@ -54,5 +51,5 @@ df<-na.omit(df)
 df= df %>% mutate(reoffer_unchosen_oneback = (lag(unchosen_card) == left_offer | lag(unchosen_card) ==right_offer),
                   stay_unchosen = lag(unchosen_card) == choice_card)
 
-save(df, file='G:/My Drive/Xbox/data/empirical_data/df.rdata')
+save(df, file='./data/empirical_data/df.rdata')
 
